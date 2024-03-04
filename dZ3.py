63caa8225921d51a15e61c1035c2e9dd2c226f9f
mikhail <118197081+duckbanny@users.noreply.github.com>
2024-03-04 09:44:33 +0400

    Add files via upload

--- dZ3.py
+++ dZ3.py
@@ -40,46 +40,60 @@
 #     print(f"Количество гласных букв: {vowelcount}")
 #     print(f"Количество согласных букв: {consonantcount}") 
 
-word = input()
-xa = word.count("a")
-xe = word.count("e")
-xo = word.count("o")
-xu = word.count("u")
-xi = word.count("i")
-countwordG = xa + xe + xo + xu + xi
-countwordS = len(word) - countwordG
-print("гласные", countwordG)
-print("согласные", countwordS)
-if xa != 0:
-    print("a =", xa)
-else:
-    print("a =", False)
-if xe != 0:
-    print("e =", xe)
-else:
-    print("e =", False)    
-if xo != 0:
-    print("o =", xo)
-else:
-    print("o =", False)
-if xu != 0:
-    print("u =", xu)
-else:
-    print("u =", False)
-if xi != 0:
-    print("i =", xi)
-else:
-    print("i =", False)     
+# word = input()
+# xa = word.count("a")
+# xe = word.count("e")
+# xo = word.count("o")
+# xu = word.count("u")
+# xi = word.count("i")
+# countwordG = xa + xe + xo + xu + xi
+# countwordS = len(word) - countwordG
+# print("гласные", countwordG)
+# print("согласные", countwordS)
+# if xa != 0:
+#     print("a =", xa)
+# else:
+#     print("a =", False)
+# if xe != 0:
+#     print("e =", xe)
+# else:
+#     print("e =", False)    
+# if xo != 0:
+#     print("o =", xo)
+# else:
+#     print("o =", False)
+# if xu != 0:
+#     print("u =", xu)
+# else:
+#     print("u =", False)
+# if xi != 0:
+#     print("i =", xi)
+# else:
+#     print("i =", False)     
 
-#gpt
-word = input()
-vowels = 'a', 'e', 'i', 'o', 'u'
-vowelcounts = {vowel: word.count(vowel) for vowel in vowels}
-consonantcount = len(word) - sum(vowelcounts.values())
-print("гласные", sum(vowelcounts.values()))
-print("согласные", consonantcount)
-for vowel, count in vowelcounts.items():
-    if count != 0:
-        print(f"{vowel} =", count)
-    else:
-        print(f"{vowel} =", False)
+# #gpt
+# word = input()
+# vowels = 'a', 'e', 'i', 'o', 'u'
+# vowelcounts = {vowel: word.count(vowel) for vowel in vowels}
+# consonantcount = len(word) - sum(vowelcounts.values())
+# print("гласные", sum(vowelcounts.values()))
+# print("согласные", consonantcount)
+# for vowel, count in vowelcounts.items():
+#     if count != 0:
+#         print(f"{vowel} =", count)
+#     else:
+#         print(f"{vowel} =", False)
+
+start_t = int(input())
+ivan = int(input())
+mike = int(input())
+if mike >= start_t and ivan >= start_t:
+    print("2")
+elif ivan >= start_t and mike < start_t:
+    print("ivan")
+elif ivan < start_t and mike >= start_t:
+    print("mike")
+elif (ivan < start_t) and (mike < start_t) and (ivan + mike)>= start_t:
+    print("1")
+elif (ivan < start_t) and (mike < start_t) and (ivan + mike) < start_t:
+    print("0")
