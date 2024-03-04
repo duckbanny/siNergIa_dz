08e9b3db34ae93025dd9b340c2d0c4785c76174e
mikhail <118197081+duckbanny@users.noreply.github.com>
2024-03-04 10:07:37 +0400

    Add files via upload

--- dZ.py
+++ dZ.py
@@ -1,22 +1,22 @@
-Введите вид питомца, его возраст и кличку
+#Введите вид питомца, его возраст и кличку
 
-a_pet, age, name_animal = input(), input(), input()
-print("Это", a_pet, "по кличке", name_animal, "Возраст:", age, "года.")
+# a_pet, age, name_animal = input(), input(), input()
+# print("Это", a_pet, "по кличке", name_animal, "Возраст:", age, "года.")
 
 
-тест по биологии для студентов.Напишите по порядку 9 этапов развития человека
+#тест по биологии для студентов.Напишите по порядку 9 этапов развития человека
 
-a1 = "Ardipithecus"
-a2 = "Australopithecus"
-a3 = "Paranthropus"
-a4 = "A skilled man"
-a5 = "The man is upright"
-a6 = "The man is upright"
-a7 = "The Flores man"
-a8 = "Neanderthals"
-a9 = "Homo sapiens"
+# a1 = "Ardipithecus"
+# a2 = "Australopithecus"
+# a3 = "Paranthropus"
+# a4 = "A skilled man"
+# a5 = "The man is upright"
+# a6 = "The man is upright"
+# a7 = "The Flores man"
+# a8 = "Neanderthals"
+# a9 = "Homo sapiens"
 
-print(a1, a2, a3, a4, a5, a6, a7, a8, a9, sep="=>")
+# print(a1, a2, a3, a4, a5, a6, a7, a8, a9, sep="=>")
 
 
 
