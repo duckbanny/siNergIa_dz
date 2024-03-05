9e07bb3dcdd322203bc0c127bfe1feb8a9f56427
mikhail <118197081+duckbanny@users.noreply.github.com>
2024-03-05 10:48:30 +0400

    Add files via upload

--- dZ4.py
+++ dZ4.py
@@ -1,2 +1,13 @@
 number = int(input())
 counter = 0
+for i in range(number):
+    number1 = int(input())
+    if number1 == 0:
+        counter += 1
+print(counter)     
+
+counter = sum(1 for _ in range(int(input())) if int(input()) == 0)
+print(counter)
+
+number = int(input())
+counter = 0
