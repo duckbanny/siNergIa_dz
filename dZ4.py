d6f08ed4d4d1f4adae8aeb19c607b5c6a80b1624
mikhail <118197081+duckbanny@users.noreply.github.com>
2024-03-10 23:57:04 +0400

    Add files via upload

--- dZ4.py
+++ dZ4.py
@@ -1,28 +1,24 @@
-# number = int(input())
-# counter = 0
-# for i in range(number):
-#     number1 = int(input())
-#     if number1 == 0:
-#         counter += 1
-# print(counter)     
+# num = int(input())
+# count = 0
+# for i in range(1, num + 1):
+#     x = int(input())
+#     if x == 0:
+#         count += 1
+# print(count)        
 
-# counter = sum(1 for _ in range(int(input())) if int(input()) == 0)
-# print(counter)
 
-# number = int(input())
-# counter = 0
-# for i in range(1, number + 1):
-#     if number % i == 0:
-#         counter += 1
-# print(counter)   
+# x = int(input())
+# y = 0
+# count = 0
+# while x >= y:
+#     y += 1
+#     if x % y == 0:
+#         count += 1
+# print(count)
 
-# # с помощью while
-# number = int(input())
-# counter = 0
-# i = 1
-# while i <= number:
-#     if number % i == 0:
-#         counter += 1
-#     i += 1
-# print(counter)
-
+# A = int(input())
+# B = int(input())
+# for num in range(A, B + 1):
+#     if num % 2 == 0:
+#         print(num, end=" ")
+    
