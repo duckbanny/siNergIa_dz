b9a08c3506b020904fbd227fc3bb8d95afa17a55
mikhail <118197081+duckbanny@users.noreply.github.com>
2024-03-11 10:13:53 +0400

    Add files via upload

---
+++ dZ5.py
@@ -0,0 +1,15 @@
+# N = int(input())
+# numbers = []
+# for _ in range(N):
+#     number = int(input())
+#     numbers.append(number)
+# reversed_numbers = numbers[::-1]
+# for number in reversed_numbers:
+#     print(number)
+# print(reversed_numbers)
+
+# num = int(input())
+# num1 = list(map(int, input().split()))
+# newNum = [num1[-1]] + num1[:-1]
+# print(newNum)
+
