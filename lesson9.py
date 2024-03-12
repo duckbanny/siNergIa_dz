b27ee646d0d1d9e244e7b789420bc19a5300b760
mikhail <118197081+duckbanny@users.noreply.github.com>
2024-03-12 14:31:01 +0400

    Add files via upload

--- lesson9.py
+++ lesson9.py
@@ -1,6 +1,24 @@
-num = int(input())
-num1 = set()
-for i in range(num):
-    num3 = int(input())
-    num1.add(num3)
-print(len(num1))
+# def new_func():
+#     num = int(input())
+#     num1 = set()
+#     for i in range(num):
+#         num3 = int(input())
+#         num1.add(num3)
+#     print(len(num1))
+#
+# new_func()
+
+
+# list1=set(input().split())
+# list2=set(input().split())
+# print (len(list1.intersection(list2)))
+
+
+# n = set()
+# words = input().split()
+# for i in words:
+#     if i not in n:
+#         n.add(i)
+#         print('NO')
+#     else:
+#         print('YES')
