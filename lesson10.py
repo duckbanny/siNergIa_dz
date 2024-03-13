8f362fcb797c9ac3924f5164e657b64ffe63af94
mikhail <118197081+duckbanny@users.noreply.github.com>
2024-03-13 22:28:30 +0400

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,18 @@
+
+# pets = {input(): {'Вид питомца': input(), 'Возраст питомца': input(), 'Имя владельца': input()}}
+
+# for kl, info in pets.items():
+#     print("\nИмя питомца:", kl)    
+#     for key in info:
+#         print(key + ':', info[key])
+# print(pets)
+
+
+# x = {10: -5}
+
+# new_x = {}
+# for key in range(10, -6, -1):  
+#     new_x[key] = key ** key
+
+# for key, value in new_x.items():
+#     print(f"{key}: {value}")       
